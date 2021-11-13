396b87ec34ee5318fb74dc6ede94a89c85eeea63
QnValerie <94223755+QnValerie@users.noreply.github.com>
2021-11-13 21:31:36 +0800

    Added Program.cs

--- Program.cs
+++ Program.cs
@@ -3,22 +3,22 @@ using System.IO;
 using System.Linq;
 using System.Collections.Generic;
 
-namespace Problem3_LAWAAN
+namespace Problem3_Qvcl
 {
 
     public static class Program 
     {
         public static void Main(string[] args) 
         {
-           Console.WriteLine(" Welcome to Employee Time Keeping System");
+           Console.WriteLine("Welcome to Employee Time Keeping System");
            
-            Console.WriteLine("*****************************************************");
+            Console.WriteLine();
             
             Console.WriteLine($"Today's Date: {DateTime.Today.ToShortDateString()}");
 
             Console.WriteLine();
 
-            //time-in
+            
 
             Console.Write("Time-in Enter your employee id:");
             string employeeId = Console.ReadLine();
@@ -26,11 +26,9 @@ namespace Problem3_LAWAAN
             TimeSpan timeIn = new TimeSpan(8, 0, 0);
             Console.WriteLine($"Your login time is recorded: {timeIn}");
 
-            //time-out
-
-            Console.WriteLine("*********************************");
+           
             Console.WriteLine();
-
+   
             Console.Write("To log your time-out enter your employee id:");
             employeeId = Console.ReadLine();
 
@@ -41,21 +39,20 @@ namespace Problem3_LAWAAN
             Console.WriteLine("*********************************");
             Console.WriteLine();
 
-            //lunch break duration
+            
 
             TimeSpan lunchBreakDuration = new TimeSpan(1, 0, 0);
 
-            //total number of hours employee worked
 
             TimeSpan totalHours = (timeOut - timeIn) - lunchBreakDuration;
             Console.WriteLine($"Your total hours worked is: {totalHours}");
 
-            //total office working hours
+          
 
             TimeSpan regularHoursStart = new TimeSpan(8, 0, 0);
             TimeSpan regularHoursEnd = new TimeSpan(17, 0, 0);
 
-            //late time-in computation
+           
 
             TimeSpan lateIn = new TimeSpan(0, 0, 0);
 
@@ -64,7 +61,7 @@ namespace Problem3_LAWAAN
                 lateIn = timeIn - regularHoursStart;
             }
 
-            //undertime time-out computation
+            
 
             TimeSpan underTime = new TimeSpan(0, 0, 0);
             TimeSpan totalUndertime = new TimeSpan(0, 0, 0);
@@ -77,7 +74,7 @@ namespace Problem3_LAWAAN
             }
 
            
-            //total of regular hours an employee has fulfilled
+            
 
             TimeSpan totalRegularHours = totalHours - (lateIn - underTime);
 
@@ -88,5 +85,3 @@ namespace Problem3_LAWAAN
         }
     }
 }
-       
-       
